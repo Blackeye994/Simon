0e0556332053955eddc0eb63f3126305143f51d6
Blackeye994 <msamad09874821@gmail.com>
2024-12-03 21:45:12 +0500

    Add something

--- JavaScript.js
+++ JavaScript.js
@@ -76,6 +76,109 @@
 //     key = false;
 // }
 
+// let boxColor = ["brown", "purple", "yellowgreen", "blueviolet"];
+
+// let user = [];
+// let Simon = [];
+// let eventTriggered = false;
+// let level = 0;
+
+
+// let body = document.querySelector("body");
+// let btn = document.querySelector("#btn");
+
+// // Event listener for starting the game on touch or click
+// btn.addEventListener("touchstart", (event)=>{
+//     if(eventTriggered == false){
+//         startGame(event);
+//     }
+// });
+
+// btn.addEventListener("click", (event)=>{
+//     if(eventTriggered == false){
+//         startGame(event);
+//     }
+// });
+
+// btn.addEventListener("keydown", (event)=>{
+//     if(eventTriggered == false){
+//         startGame(event);
+//     }
+// });
+
+// function startGame(event) {
+//     if(eventTriggered == false){
+//         eventTriggered = event.type;
+//         console.log(eventTriggered);
+//         startChange();
+//     }
+ 
+// }
+
+// function randChangeColor(rand) {
+//     rand.classList.add("color");
+//     setTimeout(() => {
+//         rand.classList.remove("color");
+//     }, 1000);
+// }
+
+// function ColorChanges() {
+//     let button = this;
+//     let color = this.getAttribute("id");
+//     randChangeColor(button);
+//     console.log(color);
+//     user.push(color);
+//     check(user.length - 1);
+// }
+
+// function check(index) {
+//     if (user[index] == Simon[index]) {
+//         if (user.length == Simon.length) {
+//             startChange(); 
+//         }
+//     } else {
+//         let h2 = document.querySelector("h2");
+//         h2.innerHTML = `Game over! Your score is <b>${level}</b> <br>Press any key or click to restart the Game`; 
+//         reset();
+//     }
+// }
+
+// function startChange() {
+//     user = [];
+//     level++;
+//     let h2 = document.querySelector("h2");
+//     h2.innerText = `Level ${level}`;
+
+//     let index = Math.floor(Math.random() * 4);
+//     let randomColor = boxColor[index];
+//     let box_choice = document.querySelector(`.${randomColor}`);
+
+//     randChangeColor(box_choice);
+//     Simon.push(randomColor);
+
+//     let box_containers = document.querySelectorAll(".box");
+ 
+//         for (box of box_containers) {
+//             console.log("Hello world!");
+//             if(eventTriggered == "touchstart"){
+//                 console.log(" am in the touch");
+//                 box.addEventListener("touchstart", ColorChanges);
+//             }else{
+//                 console.log("I am in the click and key pressing ");
+//                 box.addEventListener("click", ColorChanges);
+//             }
+//         }
+// }
+
+// function reset() {
+//     level = 0;
+//     user = [];
+//     Simon = [];
+//      eventTriggered = false;
+// }
+
+
+
 let boxColor = ["brown", "purple", "yellowgreen", "blueviolet"];
 
 let user = [];
@@ -83,35 +186,27 @@ let Simon = [];
 let eventTriggered = false;
 let level = 0;
 
-
-let body = document.querySelector("body");
+let btn = document.querySelector("#btn");
 
 // Event listener for starting the game on touch or click
-body.addEventListener("touchstart", (event)=>{
-    if(eventTriggered == false){
+btn.addEventListener("pointerdown", (event) => {
+    if (!eventTriggered) {
         startGame(event);
     }
 });
 
-body.addEventListener("click", (event)=>{
-    if(eventTriggered == false){
-        startGame(event);
-    }
-});
-
-body.addEventListener("keydown", (event)=>{
-    if(eventTriggered == false){
-        startGame(event);
-    }
-});
+// document.addEventListener("keydown", (event) => {
+//     if (eventTriggered) {
+//         reset(); // Allow restarting the game with any key after it has started
+//     }
+// });
 
 function startGame(event) {
-    if(eventTriggered == false){
-        eventTriggered = event.type;
-        console.log(eventTriggered);
+    if (!eventTriggered) {
+        eventTriggered = true;
+        console.log(event.type);
         startChange();
     }
- 
 }
 
 function randChangeColor(rand) {
@@ -131,8 +226,8 @@ function ColorChanges() {
 }
 
 function check(index) {
-    if (user[index] == Simon[index]) {
-        if (user.length == Simon.length) {
+    if (user[index] === Simon[index]) {
+        if (user.length === Simon.length) {
             startChange(); 
         }
     } else {
@@ -156,25 +251,18 @@ function startChange() {
     Simon.push(randomColor);
 
     let box_containers = document.querySelectorAll(".box");
- 
-        for (box of box_containers) {
-            console.log("Hello world!");
-            if(eventTriggered == "touchstart"){
-                console.log(" am in the touch");
-                box.addEventListener("touchstart", ColorChanges);
-            }else{
-                console.log("I am in the click and key pressing ");
-                box.addEventListener("click", ColorChanges);
-            }
-        }
+
+    // Add event listeners for the current level
+    for (let box of box_containers) {
+        box.addEventListener("pointerdown", ColorChanges);
+    }
 }
 
 function reset() {
     level = 0;
     user = [];
     Simon = [];
-    // eventTriggered = false;
+    eventTriggered = false;
+    let h2 = document.querySelector("h2");
+    h2.innerText = "Press the button to start the game"; // Reset message
 }
-
-
-
